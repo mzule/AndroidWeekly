14c66ef05f55e001c4aadba54526d293873eadde
caodongping <caodongping@baidu.com>
2016-03-24 20:49:31 +0800

    pinned section

--- app/src/main/java/com/github/mzule/androidweekly/ui/adapter/ArticleAdapter.java
+++ app/src/main/java/com/github/mzule/androidweekly/ui/adapter/ArticleAdapter.java
@@ -3,6 +3,7 @@ package com.github.mzule.androidweekly.ui.adapter;
 import android.content.Context;
 
 import com.github.mzule.androidweekly.entity.Article;
+import com.github.mzule.androidweekly.ui.view.PinnedSectionListView;
 import com.github.mzule.androidweekly.ui.viewtype.ArticleViewType;
 import com.github.mzule.androidweekly.ui.viewtype.SectionViewType;
 import com.github.mzule.easyadapter.TypePerEntityAdapter;
@@ -10,7 +11,7 @@ import com.github.mzule.easyadapter.TypePerEntityAdapter;
 /**
  * Created by CaoDongping on 3/24/16.
  */
-public class ArticleAdapter extends TypePerEntityAdapter<Object> {
+public class ArticleAdapter extends TypePerEntityAdapter<Object> implements PinnedSectionListView.PinnedSectionListAdapter {
 
     public ArticleAdapter(Context context) {
         super(context);
@@ -22,4 +23,8 @@ public class ArticleAdapter extends TypePerEntityAdapter<Object> {
         mapEntityViewType(Article.class, ArticleViewType.class);
     }
 
+    @Override
+    public boolean isItemViewTypePinned(int viewType) {
+        return getRawViewType(SectionViewType.class) == viewType;
+    }
 }
