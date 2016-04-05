03aeb9146f1b49280303734c4286097fdfc5035e
caodongping <caodongping@baidu.com>
2016-04-05 15:42:13 +0800

    search result display

--- app/src/main/java/com/github/mzule/androidweekly/ui/activity/SearchResultActivity.java
+++ app/src/main/java/com/github/mzule/androidweekly/ui/activity/SearchResultActivity.java
@@ -2,11 +2,20 @@ package com.github.mzule.androidweekly.ui.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.github.mzule.androidweekly.R;
+import com.github.mzule.androidweekly.dao.ArticleDao;
+import com.github.mzule.androidweekly.entity.Article;
+import com.github.mzule.androidweekly.ui.adapter.ArticleAdapter;
 import com.github.mzule.androidweekly.ui.view.NaviBar;
 
+import java.util.List;
+
 import butterknife.Bind;
+import butterknife.OnItemClick;
 
 /**
  * Created by CaoDongping on 4/5/16.
@@ -14,6 +23,8 @@ import butterknife.Bind;
 public class SearchResultActivity extends BaseActivity {
     @Bind(R.id.naviBar)
     NaviBar naviBar;
+    @Bind(R.id.listView)
+    ListView listView;
 
     public static Intent makeIntent(Context context, String q) {
         Intent intent = new Intent(context, SearchResultActivity.class);
@@ -21,10 +32,22 @@ public class SearchResultActivity extends BaseActivity {
         return intent;
     }
 
+    @OnItemClick(R.id.listView)
+    void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Article article = (Article) parent.getAdapter().getItem(position);
+        startActivity(ArticleActivity.makeIntent(this, article));
+    }
+
     @Override
     protected void afterInject() {
         String q = getIntent().getStringExtra("q");
         naviBar.setLeftText(q.toUpperCase());
+
+        ArticleDao articleDao = new ArticleDao();
+        List<Article> result = articleDao.search(q);
+        ArticleAdapter adapter = new ArticleAdapter(this);
+        listView.setAdapter(adapter);
+        adapter.addAndNotify(result);
     }
 
     @Override
